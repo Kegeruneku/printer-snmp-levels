5183eed6f35b1c6129cbe8b208d38e8a27c925b5
Matthieu CERDA <matthieu.cerda@normation.com>
2014-11-03 17:14:16 +0100

    Initial code import

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ hp-snmp-levels
 ==============
 
 Get HP printer cartridge levels using SNMP
+
+# Requirements
+
+This utility requires nothing that is not in python standard library.
+
+It has been tested with 2.7.8.
+
+# Usage
+
+```
+usage: hp-snmp-levels.py [-h] host community
+
+Get HP printer cartridge levels using SNMP
+
+positional arguments:
+  host        The IP address or hostname of the printer
+  community   The SNMP community to use (often 'public')
+```
+
+# Supported printers
+
+This utility has been tested with the following printer models:
+
+* HP LaserJet CM1415fn
+
+# Known issues
+
+* Will not autodetect the number of cartridges yet (assumes 4 for now)
+* Code lacks DRYness
---
+++ hp-snmp-levels.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# Get HP printer cartridge levels using SNMP
+
+# Imports
+import argparse
+import re
+import netsnmp
+import time
+from datetime import date
+
+# Functions
+def getmib(mibentry):
+
+  oid = netsnmp.Varbind(mibentry)
+  res = netsnmp.snmpget(oid, Version = 1, DestHost=host, Community=community)
+  return res[0]
+
+def getdetails(host, community):
+
+  # HP ETHERNET MULTI-ENVIRONMENT,SN:XXXXXXXXXX,FN:XXXXXXX,SVCID:XXXXX,PID:HP LaserJet CM1415fn
+
+  details = dict()
+
+  # sysName.0
+  details['name']    = netsnmp.snmpget(netsnmp.Varbind('.1.3.6.1.2.1.1.5.0'), Version = 1, DestHost=host, Community=community)[0]
+
+  # sysContact.0
+  details['contact'] = netsnmp.snmpget(netsnmp.Varbind('.1.3.6.1.2.1.1.4.0'), Version = 1, DestHost=host, Community=community)[0]
+
+  # sysUpTimeInstance
+  details['uptime'] = int(netsnmp.snmpget(netsnmp.Varbind('.1.3.6.1.2.1.1.3.0'), Version = 1, DestHost=host, Community=community)[0]) / 100
+
+  # sysDescr.0
+  res = netsnmp.snmpget(netsnmp.Varbind('.1.3.6.1.2.1.1.1.0'), Version = 1, DestHost=host, Community=community)
+
+  match = re.search(r'HP ETHERNET MULTI-ENVIRONMENT,SN:(.*),FN:(.*),SVCID:(.*),PID:(.*)', res[0])
+
+  if match:
+
+    details['sn']    = match.group(1)
+    details['fn']    = match.group(2)
+    details['svcid'] = match.group(3)
+    details['pid']   = match.group(4)
+
+    return details
+
+def getcartridgelevels(host, community):
+
+  cartridge_number = 5
+
+  res = dict()
+
+  for i in range(1, cartridge_number):
+
+    res[i] = dict()
+
+    res[i]['name'] = netsnmp.snmpget(netsnmp.Varbind(".1.3.6.1.2.1.43.11.1.1.6.1." + str(i)), Version = 1, DestHost=host, Community=community)[0]
+    res[i]['level'] = netsnmp.snmpget(netsnmp.Varbind(".1.3.6.1.2.1.43.11.1.1.9.1." + str(i)), Version = 1, DestHost=host, Community=community)[0]
+
+  return res
+
+# Runtime
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser(description='Get HP printer cartridge levels using SNMP')
+  parser.add_argument('host', help="The IP address or hostname of the printer")
+  parser.add_argument('community', help="The SNMP community to use (often 'public')")
+  args = parser.parse_args()
+
+  host = args.host
+  community = args.community
+
+  details = getdetails(host, community)
+
+  print "This is a " + details['pid'] + " printer, named " + details['name'] + " and with serial no. " + details['sn'] + ", up since the " + str(date.fromtimestamp(time.time() - int(details['uptime']))) + "\n"
+
+  levels = getcartridgelevels(host, community)
+
+  print "Cartridge levels:"
+
+  for key in levels:
+
+    print levels[key]["name"] + " has level " + levels[key]["level"] + "%"
+
+  print "\nPlease contact " + details['contact'] + " for details."
